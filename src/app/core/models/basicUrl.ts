ff3c886cf1121eab846b7efdf3097402db4d245e
Yevhenii Bezeha <bezegajenja0305@gmail.com>
2022-01-30 20:16:28 +0200

    asdfg

--- src/app/core/models/basicUrl.ts
+++ src/app/core/models/basicUrl.ts
@@ -3,7 +3,7 @@ import { environment } from '../../../environments/environment.prod';
 let url = '';
 
 if (environment.production) {
-  url = 'https://nameless-anchorage-78678.herokuapp.com';
+  url = 'https://nameless-anchorage-78678.herokuapp.com/api';
 } else {
   url = 'http://localhost:3000/api';
 }
