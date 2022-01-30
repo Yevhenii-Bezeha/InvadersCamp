66330f4828ee1da76c07825e8e703f2f24b57b06
Yevhenii Bezeha <bezegajenja0305@gmail.com>
2022-01-30 20:10:48 +0200

    add

--- src/app/core/models/basicUrl.ts
+++ src/app/core/models/basicUrl.ts
@@ -3,9 +3,9 @@ import { environment } from '../../../environments/environment.prod';
 let url = '';
 
 if (environment.production) {
-  url = 'http://localhost:3000/api';
-} else {
   url = 'https://nameless-anchorage-78678.herokuapp.com';
+} else {
+  url = 'http://localhost:3000/api';
 }
 
 export const basicUrl = url;
